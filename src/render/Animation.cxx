e456147f6308e2fa19be5945f507c1e54be59afb
robbiejdunn <robbiejdunn94@gmail.com>
2022-10-23 13:09:12 +0100

    sprite changes

---
+++ src/render/Animation.cxx
@@ -0,0 +1,17 @@
+// #include "Animation.h"
+
+// Animation::Animation(std::vector<std::unique_ptr<Sprite>> sprites)
+// :sprites (sprites)
+// {
+
+// }
+
+// Animation::~Animation()
+// {
+
+// }
+
+// void Animation::render(int frameNumber, SDL_Renderer *renderer, int x, int y, double scale = 1.0)
+// {
+//     // sprites[frameNumber]->render(renderer, x, y, scale);
+// }
